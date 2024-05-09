71ecfda7017b7c5445cd8dc768a042fa95088881
Mathewsq369 <mathewsq369@gmail.com>
2024-05-09 22:31:49 +0300

    implemented sort function

--- selectionSort.c
+++ selectionSort.c
@@ -17,6 +17,28 @@ int main()
 
 void sort(int a[], int size)
 {
+    int hold = 0, smallest = 0, swap = 0;
+
+    for (size_t i = 0; i < size; i++)
+    {
+        swap = 0;
+        hold = 0;
+        smallest = i;
+        for (size_t j = i; j < size; j++)
+        {
+            if (a[smallest] > a[j])
+            {
+                smallest = j;
+                swap++;
+            }
+        }
+        if(swap > 0)
+        {
+            hold = a[i];
+            a[i] = a[smallest];
+            a[smallest] = hold;
+        }
+    }
 }
 
 void printArray(int a[], int size)
